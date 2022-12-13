75eb76f1219772e57ae2f5a69a93fdaa2941e23c
Sziku <105989158+Sziku@users.noreply.github.com>
2022-12-13 09:55:32 +0100

    add jdbc and first three test passed

--- src/main/java/hu/nive/ujratervezes/plants/PlantManager.java
+++ src/main/java/hu/nive/ujratervezes/plants/PlantManager.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.plants;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlantManager {
 
     private final String dbUrl;
@@ -10,5 +14,25 @@ public class PlantManager {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
+    }
+
+    public List<String> getPlantsToBeWatered() {
+        final String sql = "select plant_name from plants p join watered w on p.plant_id = w.plant_id where w.watered_at < '2022-01-01' order by p.plant_name;";
+
+        try(Connection con = DriverManager.getConnection(dbUrl, dbUser, dbPassword)){
+            PreparedStatement st = con.prepareStatement(sql);
+
+            ResultSet rs = st.executeQuery();
+            List<String> resultList = new ArrayList<>();
+            while (rs.next()){
+                resultList.add(rs.getString(1));
+            }
+
+            return resultList;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
